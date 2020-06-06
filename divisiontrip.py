864ccd28acbaaf224745d5a5f6e8b17c71dd8f4c
Christiantayu <49259113+Christiantayu@users.noreply.github.com>
2020-06-06 16:40:29 -0500

    Add files via upload

---
+++ divisiontrip.py
@@ -0,0 +1,9 @@
+Python 3.7.2rc1 (tags/v3.7.2rc1:75a402a217, Dec 11 2018, 23:05:39) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 5
+>>> b = 3
+>>> c = 20
+>>> d =a/b/c
+>>> print(d)
+0.08333333333333334
+>>> 
